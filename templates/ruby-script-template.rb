594a4df251a98dfb8012e884512d3c6cb0d2a2e1
que <qs5779@mail.com>
2018-07-06 13:27:37 -0400

    added getExec

--- templates/ruby-script-template.rb
+++ templates/ruby-script-template.rb
@@ -4,3 +4,32 @@
 # Revision History:
 # YYYYmmdd - whoami - initial version
 #
+
+def getExec(cmd, rc)
+
+
+  printf ("[executing] %s\n", $cmd if @debug
+
+  # NOTE: if the script must run on SunOS and the command uses a sub shell convention like "dirname $(<appl>SUBDIR -I)"
+  #       then you need to wrap it with [ /bin/ksh -c "command with double quotes escaped" ]
+  #       the reason is that "sh" on SunOS does not support the sub shell convention indicated above
+
+  $v = qx/$c/;
+  $rc = $?;
+
+  if (defined($rcref)) {
+    $$rcref = $rc;
+  }
+
+  # trim leading/trailing spaces
+  $v =~ s/^\s+//;
+  $v =~ s/\s+$//;
+
+  chomp($v);
+  return $v;
+}
+
+def doExec(cmd)
+  getExec(cmd, rc)
+  rc
+end
