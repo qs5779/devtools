dcb9295a14c8f5289734e32693965dda7c7c0f1c
Quien Sabe <qs5779@mail.com>
2019-12-24 16:14:23 -0500

    Fixed rubocop / logic issues.

--- templates/ruby-script-template.rb
+++ templates/ruby-script-template.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/ruby
-# -*- Mode: Ruby; tab-width: 2; indent-tabs-mode: nil -*- vim:sta:et:sw=2:ts=2:syntax=ruby
+# frozen_string_literal: true
+
+# vim:sta:et:sw=2:ts=2:syntax=ruby
 #
 # Revision History:
 # YYYYmmdd - whoami - initial version
@@ -7,89 +9,67 @@
 
 require 'getoptlong'
 
-basenm = File.basename( $0 );
-SCRDIR = File.dirname( $0 );
+basenm = File.basename($PROGRAM_NAME)
+# scrdir = File.dirname($PROGRAM_NAME)
 
 # TODO: convert below perl to ruby
 
 debug = 0
 verbose = 0
-man = 0
-help = 0
-version = 0
 
-ERRORS = 0
-VERSION = '$Revision: 24951 $'
+errors = 0
+VERSION = '0.0.1'
 
 opts = GetoptLong.new(
-  [ '--debug', '-d', GetoptLong::NO_ARGUMENT ],
-  [ '--verbose', '-v', GetoptLong::NO_ARGUMENT ],
-  [ '--version', '-V', GetoptLong::NO_ARGUMENT ],
-  [ '--help', '--man', '-h', GetoptLong::NO_ARGUMENT ],
-#  [ '--repeat', '-n', GetoptLong::REQUIRED_ARGUMENT ],
-#  [ '--name', GetoptLong::OPTIONAL_ARGUMENT ]
+  ['--debug', '-d', GetoptLong::NO_ARGUMENT],
+  ['--verbose', '-v', GetoptLong::NO_ARGUMENT],
+  ['--version', '-V', GetoptLong::NO_ARGUMENT],
+  ['--help', '--man', '-h', GetoptLong::NO_ARGUMENT]
+  #  [ '--repeat', '-n', GetoptLong::REQUIRED_ARGUMENT ],
+  #  [ '--name', GetoptLong::OPTIONAL_ARGUMENT ],
 )
 
-opts.each do |opt, arg|
+opts.each do |opt, _arg|
   case opt
-    when '--help'
-      puts <<-EOF
-hello [OPTION] ... DIR
-
--h, --help:
-   show help
-
---repeat x, -n x:
-   repeat x times
-
---name [name]:
-   greet user by name, if name not supplied default is John
-
-DIR: The directory in which to issue the greeting.
-      EOF
-      exit 0
-    when '--version'
-      puts "#{basenm} #{VERSION[/\d+/]}"
-      exit 0
-    when '--verbose'
-      verbose += 1
-    when '--debug'
-      debug += 1
+  when '--help'
+    puts <<-USG
+  usage: #{@basenm} [-d] [-h] [-v] [-V]
+    where:
+      -d - specify debug mode
+      -h - show this message and exit
+      -v - add verbosity
+      -V - show version and exit
+    USG
+    exit 0
+  when '--version'
+    puts "#{basenm} #{VERSION}"
+    exit 0
+  when '--verbose'
+    verbose += 1
+  when '--debug'
+    debug += 1
   end
 end
 
-if debug > 0
-  puts "debug: #{debug}"
-end
+puts "debug: #{debug}" if debug.positive?
 
-if verbose > 0
-  puts "verbose: #{verbose}"
-end
+puts "verbose: #{verbose}" if verbose.positive?
 
-def getExec(cmd, rc = nil)
+def get_exec(cmd)
+  puts "[executing] #{cmd}\n" if debug.positive?
 
-  if debug > 0
-    puts "[executing] #{cmd}\n"
-  end
-
-  v = %x{cmd};
-  ec = $?;
-
-  if rc != nil
-    rc[0] = ec
-  end
+  v = `cmd`
+  ec = $CHILD_STATUS
 
   v.strip!
   v.chomp!
 
-  v
+  [v, ec]
 end
 
-def doExec(cmd)
-  rc = Array.new
-  getExec(cmd, rc)
-  rc[0]
+def do_exec(cmd)
+  _ignore, ec = get_exec(cmd)
+  ec
 end
 
-
-exit ERRORS
+exit errors
